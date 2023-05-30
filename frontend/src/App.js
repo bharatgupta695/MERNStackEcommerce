56f6dde91c557e77d4714c5cd3a35bf7c08682c4
bharatgupta695 <bharatgupta68823@gmail.com>
2023-05-30 23:28:17 +0530

    Headers and footers completed(frontend)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Header from "./component/layout/Header/Header.js";
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
 import webfont from "webfontloader";
 import React from 'react';
 import Footer from './component/layout/Footer/Footer.js';
+import Home from './component/Home/Home.js';
 
  
 function App() {  
@@ -17,6 +18,9 @@ React.useEffect(() => {
   return (
     <Router>
      <Header />
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      </Routes>
      <Footer/>
      </Router>
   );
---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react'
+import { CgMouse } from "react-icons/cg";
+import "./Home.css";
+import Product from './Product.js';
+
+const product  = {
+    _id: 1,
+    name:"Product 1",
+    price:100,
+    description:"This is a product",
+    images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+    countInStock:10
+};
+
+const Home = () => {
+  return  <Fragment> 
+    <div className="banner"> 
+    <p>Welcome to Ecommerce</p>
+    <h1>Find Amazing Products Below</h1>
+     
+
+     <a href ="#container">
+        <button >
+            Scroll <CgMouse/> 
+            </button>
+     </a>
+    </div>
+    <h2 className='homeHeading'>Featured Products</h2>
+    <div className="container" id="container">
+        <Product product = {product}/>
+        <Product product = {product}/>
+        <Product product = {product}/>
+        <Product product = {product}/>
+
+        <Product product = {product}/>
+        <Product product = {product}/>
+        <Product product = {product}/>
+        <Product product = {product}/>
+         </div>
+    </Fragment>   
+  
+}
+
+export default Home
